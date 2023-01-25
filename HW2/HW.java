c05c221cf53f7d41216eb5b1155a12951e8029b0
SsYuRb <113142100+SsYuRb@users.noreply.github.com>
2023-01-25 19:39:12 +0300

    End hw2

--- HW2/HW.java
+++ HW2/HW.java
@@ -1,14 +1,16 @@
 package HW2;
 
-import java.io.FileNotFoundException;
+// import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 // import java.nio.file.Paths;
 /**
  * main
  */
 public class HW {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         FileReader fr = new FileReader("/Users/estaban/Desktop/Прогерство/java_gb/HW2/input.txt");
         Scanner sc = new Scanner(fr);
         StringBuilder ch = new StringBuilder();
@@ -23,6 +25,10 @@ public class HW {
         String a = Character.toString(array[5]);
         a = a + Character.toString(array[6]);
         int c = Integer.parseInt(a);
-        System.out.println(Math.pow(c, b));
+        FileWriter fw = new FileWriter("/Users/estaban/Desktop/Прогерство/java_gb/HW2/outptu.txt");
+        double k = Math.pow(c, b);
+        String sdf = Double.toString(k);
+        fw.write(sdf);
+        fw.close();
     }
 }

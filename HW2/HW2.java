b5726b06bc70ed6f1a0e6fb0bd8e680035922f93
SsYuRb <113142100+SsYuRb@users.noreply.github.com>
2023-01-31 15:39:47 +0300

    Version 2.

---
+++ HW2/HW2.java
@@ -0,0 +1,38 @@
+
+
+package HW2;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+import org.w3c.dom.UserDataHandler;
+
+/**
+ * HW2
+ */
+public class HW2 {
+
+    public static void main(String[] args) throws IOException {
+        FileReader fr = new FileReader("java_gb/HW2/input.txt");
+        LinkedList<Integer> ll = new LinkedList<Integer>();
+        Scanner sc = new Scanner(fr);
+        int a = 1;
+        while (sc.hasNextLine()){
+            if (sc.next()==" "); 
+                ll.add(sc.nextInt());
+        } 
+        
+        if (ll.get(0) == 0){
+            System.out.println(1);
+        } else {
+            for (int i = 0; i < ll.get(0); i++) {
+                a *= ll.getLast();
+            }
+            System.out.println(a);
+        }
+        fr.close();
+        sc.close();
+    }
+}

5a219b75af4826a2e64cf165bb63efe90f3fb14a
SsYuRb <113142100+SsYuRb@users.noreply.github.com>
2023-02-08 16:53:23 +0300

    Mazedone

--- HW4/MazeRouter.java
+++ HW4/MazeRouter.java
@@ -12,8 +12,9 @@ public class MazeRouter {
     public static int n;
     public static Scanner scanner = new Scanner(System.in);
     public static Queue<Integer[]> queueNum = new LinkedList<Integer[]>();
+    public static Queue<Integer[]> queueRet = new LinkedList<Integer[]>();
     public static Integer[] XYQ = new Integer[2];
-    public static Integer[] exiter = new Integer[2];
+    public static int[] exiter = new int[2];
 
     public static void main(String[] args) {
         int[][] arr = InitArr();
@@ -26,12 +27,13 @@ public class MazeRouter {
         System.out.println("The array of roads");
         PrintArray(arrayWithRoad);
         System.out.println("The shortest way");
-        int[][] arrayWithTheRoad = ShortWay(arrayWithRoad);
+        int[][] arrayWithTheRoad = ShortestWay(arrayWithRoad);
         PrintArray(arrayWithTheRoad);
     }
 
     public static int[][] findAWay(int[][] arr) {
-        while ((x != exiter[0]) || (y != exiter[1] - 1)) {
+        Boolean exit = true;
+        while (exit) {
             if ((x != 0) && (x - 1 >= 0)) {
                 if (arr[x - 1][y] == 0) {
                     arr[x - 1][y] = arr[x][y] + 1;
@@ -59,6 +61,9 @@ public class MazeRouter {
                     XYQ = new Integer[] { x, y - 1 };
                     queueNum.add(XYQ);
                 }
+            }
+            if (queueNum.isEmpty()) {
+                exit = false;
             }
             xyDinamic();
         }
@@ -99,7 +104,7 @@ public class MazeRouter {
         int exitX = scanner.nextInt();
         int exitY = scanner.nextInt();
         arr[exitX][exitY] = -2;
-        exiter = new Integer[] { exitX, exitY };
+        exiter = new int[] { exitX, exitY };
         return arr;
     }
 
@@ -121,112 +126,122 @@ public class MazeRouter {
     public static void PrintArray(int[][] arr) {
         for (int i = 0; i < n; i++) {
             for (int index = 0; index < n; index++) {
-                System.out.printf("  ,%d , ", arr[i][index]);
+                System.out.printf("%4d", arr[i][index]);
             }
             System.out.println();
         }
     }
 
-    public static int[][] ShortWay(int[][] arr) {
-        x = exiter[0];
-        y = exiter[1];
+    public static void fillExit(int[][] arr) {
+
+    }
+
+    public static void MakeQuere(int x, int y) {
+        if (x == 0) {
+            if (y == 0) {
+                Integer[] kk1 = new Integer[] { x, y + 1 };
+                Integer[] kk2 = new Integer[] { x + 1, y };
+                queueRet.add(kk1);
+                queueRet.add(kk2);
+                queueRet.add(kk2);
+
+            } else if (y == n - 1) {
+                Integer[] kk1 = new Integer[] { x + 1, y };
+                Integer[] kk2 = new Integer[] { x, y - 1 };
+                queueRet.add(kk1);
+                queueRet.add(kk2);
+                queueRet.add(kk2);
+
+            } else if ((y != 0) && (y != n - 1)) {
+                Integer[] kk1 = new Integer[] { x, y + 1 };
+                Integer[] kk2 = new Integer[] { x + 1, y };
+                Integer[] kl = new Integer[] { x, y - 1 };
+                queueRet.add(kk1);
+                queueRet.add(kk2);
+                queueRet.add(kl);
+                queueRet.add(kl);
+
+            }
+        } else if (x == n - 1) {
+            if (y == 0) {
+                Integer[] kk1 = new Integer[] { x - 1, y };
+                Integer[] kk2 = new Integer[] { x, y + 1 };
+                queueRet.add(kk1);
+                queueRet.add(kk2);
+                queueRet.add(kk2);
+
+            } else if (y == n - 1) {
+                Integer[] kk1 = new Integer[] { x - 1, y };
+                Integer[] kk2 = new Integer[] { x, y - 1 };
+                queueRet.add(kk1);
+                queueRet.add(kk2);
+                queueRet.add(kk2);
+
+            } else if ((y != 0) && (y != n - 1)) {
+                Integer[] kk1 = new Integer[] { x - 1, y };
+                Integer[] kk2 = new Integer[] { x, y + 1 };
+                Integer[] kk3 = new Integer[] { x, y - 1 };
+                queueRet.add(kk1);
+                queueRet.add(kk2);
+                queueRet.add(kk3);
+                queueRet.add(kk3);
+            }
+        } else if (x != 0 && x != n - 1) {
+            if (y == 0) {
+                Integer[] kk1 = new Integer[] { x - 1, y };
+                Integer[] kk2 = new Integer[] { x, y + 1 };
+                Integer[] kk3 = new Integer[] { x + 1, y };
+                queueRet.add(kk1);
+                queueRet.add(kk2);
+                queueRet.add(kk3);
+                queueRet.add(kk3);
+            } else if (y == n - 1) {
+                Integer[] kk1 = new Integer[] { x - 1, y };
+                Integer[] kk2 = new Integer[] { x + 1, y };
+                Integer[] kk3 = new Integer[] { x, y - 1 };
+                queueRet.add(kk1);
+                queueRet.add(kk2);
+                queueRet.add(kk3);
+                queueRet.add(kk3);
+            } else if ((y != 0) && (y != n - 1)) {
+                Integer[] kk1 = new Integer[] { x - 1, y };
+                Integer[] kk2 = new Integer[] { x, y + 1 };
+                Integer[] kk3 = new Integer[] { x + 1, y };
+                Integer[] kk4 = new Integer[] { x, y - 1 };
+                queueRet.add(kk1);
+                queueRet.add(kk2);
+                queueRet.add(kk3);
+                queueRet.add(kk4);
+                queueRet.add(kk4);
+            }
+        }
+    }
+
+    public static int[][] ShortestWay(int[][] arr) {
+        int x = exiter[0];
+        int y = exiter[1];
+        int min = 1000;
+        boolean ex = true;
         arr[x][y] = 100;
-        while ((x != initX) || (y != initY)) {
-            if (x == 0){
-                if (y == 0){
-                    if ((arr[x+1][y] < arr[x][y+1]) && (arr[x+1][y]!=-1)) {
-                        arr[x+1][y] = 100;
-                        x += 1;
-                    } else {
-                        arr[x][y+1] = 100;
-                        y += 1;
-                    }
-                }else if (y == exiter[1]){
-                    if ((arr[x+1][y] < arr[x][y-1]) && (arr[x+1][y]!=-1)) {
-                        arr[x+1][y] = 100;
-                        x += 1;
-                    } else {
-                        arr[x][y-1] = 100;
-                        y -= 1;
-                    }
-                }else{
-                    if ((arr[x][y+1] <= arr[x+1][y]) && (arr[x][y+1] <= arr[x][y-1]) &&(arr[x][y+1]!=-1)) {
-                        arr[x][y+1] = 100;
-                        y += 1;
-                    } else if ((arr[x+1][y] <= arr[x][y-1]) && (arr[x+1][y]!=-1)) {
-                        arr[x+1][y] = 100;
-                        x += 1;
-                    } else {
-                        arr[x][y-1] = 100;
-                        y -= 1;
-                    }
+        while (ex) {
+            queueRet.clear();
+            MakeQuere(x, y);
+            for (int i = 0; i < queueRet.size() + 1; i++) {
+                Integer[] lop = queueRet.poll();
+                if (lop[0] == initX && lop[1] == initY) {
+                    ex = false;
+                    x = lop[0];
+                    y = lop[1];
+                    arr[x][y] = 100;
+                    break;
                 }
-            } else if (x == exiter[0]) {
-                if (y == 0){
-                    if ((arr[x-1][y] <= arr[x][y+1]) && (arr[x-1][y] != -1)){
-                        arr[x-1][y] = 100;
-                        x -= 1;
-                    } else {
-                        arr[x][y+1] = 100;
-                        y += 1;
-                    }
-                }else if (y == exiter[1]){
-                    if ((arr[x-1][y] <= arr[x][y-1]) && (arr[x-1][y] != -1)) {
-                        arr[x-1][y] = 100;
-                        x -= 1;
-                    } else {
-                        arr[x][y-1] = 100;
-                        y -= 1;
-                    }
-                }else{
-                    if ((arr[x-1][y] <= arr[x][y+1]) && (arr[x-1][y] <= arr[x][y-1]) && (arr[x-1][y] !=-1)) {
-                        arr[x-1][y] = 100;
-                        x -= 1;
-                    } else if ((arr[x][y + 1] <= arr[x][y-1])&&(arr[x][y + 1]!=-1)) {
-                        arr[x][y + 1] = 100;
-                        y += 1;
-                    } else {
-                        arr[x][y-1] = 100;
-                        y -= 1;
-                    }
-                }
-            } else if ((y == 0) && (x!= 0) && (x != exiter[0])) {
-                if ((arr[x-1][y] <= arr[x][y+1]) && (arr[x-1][y] <= arr[x+1][y])&&(arr[x-1][y]!=-1)) {
-                    arr[x-1][y] = 100;
-                    x -= 1;
-                } else if ((arr[x][y+1] <= arr[x+1][y]) && (arr[x][y+1] != -1)) {
-                    arr[x][y+1] = 100;
-                    y += 1;
-                } else {
-                    arr[x+1][y] = 100;
-                    x += 1;
-                }
-            } else if ((y == exiter[1]) && (x!= 0) && (x != exiter[0])) {
-                if ((arr[x-1][y] <= arr[x+1][y]) && (arr[x-1][y] <= arr[x][y-1]) && (arr[x-1][y] != -1)) {
-                    arr[x-1][y] = 100;
-                    x -= 1;
-                } else if ((arr[x+1][y] <= arr[x][y-1]) && (arr[x+1][y]!=-1)) {
-                    arr[x+1][y] = 100;
-                    x += 1;
-                } else {
-                    arr[x][y-1] = 100;
-                    y -= 1;
-                }
-            } else {
-                if ((arr[x-1][y] <= arr[x][y+1]) && (arr[x-1][y] <= arr[x+1][y]) && (arr[x-1][y] <= arr[x][y-1]) && (arr[x-1][y] != -1)) {
-                    arr[x-1][y] = 100;
-                    x -= 1;
-                } else if ((arr[x][y+1] <= arr[x+1][y]) && (arr[x][y+1] <= arr[x][y-1])&& (arr[x][y+1]!=-1)) {
-                    arr[x][y+1] = 100;
-                    y += 1;
-                } else if ((arr[x+1][y] <= arr[x][y-1])&&(arr[x+1][y]!=-1)) {
-                    arr[x+1][y] = 100;
-                    x += 1;
-                } else {
-                    arr[x][y-1] = 100;
-                    y -= 1;
+                if (arr[lop[0]][lop[1]] < min && arr[lop[0]][lop[1]] != -1 && arr[lop[0]][lop[1]] != 0) {
+                    min = arr[lop[0]][lop[1]];
+                    x = lop[0];
+                    y = lop[1];
                 }
             }
+            arr[x][y] = 100;
         }
         return arr;
     }
